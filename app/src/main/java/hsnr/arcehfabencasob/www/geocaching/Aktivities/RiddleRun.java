1fd24b48b0b085643443587cd693ca9fc7e35f22
Assaress <mygamermail1@web.de>
2016-11-24 18:06:40 +0100

    Meine Dateien

--- app/src/main/java/hsnr/arcehfabencasob/www/geocaching/Aktivities/RiddleRun.java
+++ app/src/main/java/hsnr/arcehfabencasob/www/geocaching/Aktivities/RiddleRun.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
+import hsnr.arcehfabencasob.www.geocaching.R;
+
 /**
  * Created by Assares on 24.11.2016.
  */
@@ -13,7 +15,7 @@ public class RiddleRun extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.riddle_start);
+        setContentView(R.layout.riddle_run);
         Bundle extras = getIntent().getExtras();
         String name = extras.getString("name");
         String cp = extras.getString("cp");
