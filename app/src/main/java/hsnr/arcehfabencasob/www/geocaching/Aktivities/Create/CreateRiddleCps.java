49648e4d239f565b659ead3bfa4f8f982e10c22a
Assaress <mygamermail1@web.de>
2017-01-06 13:44:20 +0100

    Meine Dateien

--- app/src/main/java/hsnr/arcehfabencasob/www/geocaching/Aktivities/Create/CreateRiddleCps.java
+++ app/src/main/java/hsnr/arcehfabencasob/www/geocaching/Aktivities/Create/CreateRiddleCps.java
@@ -1,8 +1,26 @@
 package hsnr.arcehfabencasob.www.geocaching.Aktivities.Create;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+
+import hsnr.arcehfabencasob.www.geocaching.R;
+
 /**
  * Created by Assares on 05.01.2017.
  */
 
-public class CreateRiddleCps {
+public class CreateRiddleCps extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_riddle_cps);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.create_riddle_menu, menu);
+        return true;
+    }
 }
