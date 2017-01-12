bf2de2ad47a076af6202d2ba29ab08ecfa20cfe3
Assaress <mygamermail1@web.de>
2017-01-12 15:32:33 +0100

    Meine Dateien

--- app/src/main/java/hsnr/arcehfabencasob/www/geocaching/Aktivities/Create/CreateRiddleFinish.java
+++ app/src/main/java/hsnr/arcehfabencasob/www/geocaching/Aktivities/Create/CreateRiddleFinish.java
@@ -1,10 +1,25 @@
 package hsnr.arcehfabencasob.www.geocaching.Aktivities.Create;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import hsnr.arcehfabencasob.www.geocaching.R;
 
 /**
  * Created by Assares on 05.01.2017.
  */
 
 public class CreateRiddleFinish extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_riddle_finish);
+    }
+
+    protected void createRiddle (View view){
+        finish();
+    }
+
 }
